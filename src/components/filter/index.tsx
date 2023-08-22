e20514faeb14e79e740e19ff011670ab2c742bde
Thundasantana <ednilson.sdas@outlook.com>
2023-08-22 19:40:45 -0300

    Add Filter component

--- src/components/filter/index.tsx
+++ src/components/filter/index.tsx
@@ -1,6 +1,10 @@
 import './styles.css';
-
+import 'flatpickr/dist/themes/material_green.css';
+import flatpickrLib from 'flatpickr';
+import { Portuguese } from 'flatpickr/dist/l10n/pt';
 import FlatPicker from 'react-flatpickr';
+
+flatpickrLib.localize(Portuguese);
 
 function Filter() {
   const onChangeDate = (dates: Date[]) => {
@@ -12,12 +16,19 @@ function Filter() {
       <FlatPicker
         options={{
           mode: 'range',
-          dateFormat: 'd/m/Y'
+          dateFormat: 'd/m/Y',
+          showMonths: 2
         }}
         className="filter-input"
         onChange={onChangeDate}
         placeholder="Selecione um período"
       />
+      <select className="filter-input">
+        <option value="">Selecione um gênero</option>
+        <option value="MALE">Masculino</option>
+        <option value="FEMALE">Feminino</option>
+        <option value="OTHER">Outro</option>
+      </select>
     </div>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,11 +3,9 @@ import './styles.css';
 function Header() {
   return (
     <div className="main-header-container">
-      <h1 className="main-header-title">
-        DS Sales
-      </h1>
+      <h1 className="main-header-title">DS Sales</h1>
     </div>
-  )
+  );
 }
 
 export default Header;
